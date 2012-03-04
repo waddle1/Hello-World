0276f75f215211acfe6e9c39132cd0e57ed17a2b
waddle1 <nikhilswadhwa@gmail.com>
2012-03-04 16:54:16 -0500

    added user input for height

--- cone.java
+++ cone.java
@@ -5,10 +5,18 @@ public class cone {
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		//height of cone value of pi and 1/3
-		final int HEIGHT = 10;
 		final double PI = 3.14;
 		final double divide = .333333333333333333;
 		System.out.println("\nCone head volume");
+		//get height of cone
+		System.out.print("Enter the height: ");
+		double height = s.nextDouble();
+		//check if height is in range
+		while(HEIGHT < 0) {
+			System.out.print("\nCan not have negative radius ");
+			System.out.print("\n\nEnter a positive number: ");
+			HEIGHT = s.nextDouble();
+		}
 		// get radius of cone
 		System.out.print("\nEnter the radius:  ");
 		double radius = s.nextDouble();
